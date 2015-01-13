c4f22988e0d24de095be13ab99f122201f5b1e2a
Yeo Eng Guan <yeoenggu@gmail.com>
2015-01-13 21:44:56 +0800

    Update Post with CRUD and bootstrap

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,15 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @posts = Post.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  get 'welcome/about'
-  get 'welcome/contact'
+  resources :posts
+
+  get 'about' => 'welcome#about'
   root to: 'welcome#index'
 end

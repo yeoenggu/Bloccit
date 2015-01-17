92bfe544d6a82473573cbb95040ecc9dec9a56e8
Yeo Eng Guan <yeoenggu@gmail.com>
2015-01-17 19:42:01 +0800

    Finish checkpoint with associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,22 @@
 require 'faker'
 
+# Create Users
+5.times do 
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
+
 # Create Posts
 50.times do 
   Post.create!(
+    user: users.sample,
     title: Faker::Lorem.sentence,
     body:  Faker::Lorem.paragraph
   )
@@ -18,6 +32,12 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.skip_confirmation!
+user.update_attributes!(
+  email: 'yeoenggu@gmail.com',
+  password: 'United123'
+)
 p "Seed finished"
 p "#{Post.count} posts created"
 p "#{Comment.count} comments created"

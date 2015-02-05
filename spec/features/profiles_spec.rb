d694251d7973d77d8b9ec468d2b47b33d024de2e
Yeo Eng Guan <yeoenggu@gmail.com>
2015-02-05 13:54:30 +0800

    WIP: point where there is a bug in capybara

---
+++ spec/features/profiles_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+describe "Visiting profiles" do
+  include TestFactories
+
+  before do
+    @user = authenticated_user
+  end
+
+  describe "not signed in " do
+    it "shows profile" do
+      p user_path(@user)
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+    end
+  end
+end

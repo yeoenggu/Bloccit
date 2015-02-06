55c001ac7bfaf08e4c4d59190b0787706dc901f9
Yeo Eng Guan <yeoenggu@gmail.com>
2015-02-07 02:06:05 +0800

    Completed popular posts

---
+++ app/controllers/topics/posts_controller.rb
@@ -0,0 +1,72 @@
+class Topics::PostsController < ApplicationController
+  
+  def show
+    @topic = Topic.find(params[:topic_id])
+    authorize @topic
+    @post = Post.find(params[:id])  
+    @comments = @post.comments
+  end
+
+  def new
+    @topic = Topic.find(params[:topic_id])    
+    @post = Post.new
+    authorize @post 
+  end
+
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @post = current_user.posts.build(post_params)
+    @post.topic = @topic
+    authorize @post
+      
+    if @post.save
+      @post.create_vote
+      flash[:notice] = "Post was saved"
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error saving the post.  Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
+  end
+
+  def update
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
+    
+    if @post.update_attributes(post_params)
+      flash[:notice] = "Post was updated"
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error saving the post.  Please try again."
+      render :new
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
+
+    if @post.destroy
+      flash[:notice] = "Comment deleted..."
+      redirect_to @topic
+    else
+      flash[:error] = "Error in deleting comment..."
+      render :show
+    end    
+  end
+  
+  #############
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :image)
+  end
+end
